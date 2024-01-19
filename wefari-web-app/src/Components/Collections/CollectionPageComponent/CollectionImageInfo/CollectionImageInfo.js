5392ec0ab30df628a79ef7ade8c1ea8f65eba767
Tim <hsin.cheng.tw1@gmail.com>
2024-01-20 02:11:16 +0800

    Very-Hard-Parallax-scrolling

--- wefari-web-app/src/Components/Collections/CollectionPageComponent/CollectionImageInfo/CollectionImageInfo.js
+++ wefari-web-app/src/Components/Collections/CollectionPageComponent/CollectionImageInfo/CollectionImageInfo.js
@@ -4,7 +4,7 @@ import './CollectionImageInfo.css'; // Import your CSS file
 const CollectionImageInfo = ({ imageUrl, title, placeCount }) => {
   return (
     <div className="collection-image-block">
-      <img className="collection-image" src={'image1.jpg'} alt="Collection" />
+      {/* <img className="collection-image" src={'image1.jpg'} alt="Collection" /> */}
       <div className="collection-info">
         <div className="place-count">{`${placeCount} places`}</div>
         <div className="collection-title">{title}Costa Rica</div>
--- wefari-web-app/src/Components/Collections/CollectionPageComponent/CollectionPageComponent.js
+++ wefari-web-app/src/Components/Collections/CollectionPageComponent/CollectionPageComponent.js
@@ -6,35 +6,46 @@ import PlaceInfo from './PlaceInfo/PlaceInfo';
 
 import './CollectionPageComponent.css';
 
+const whiteBGstyle={
+    backgroundColor: 'white',
+}
+
 const CollectionPageComponent = () => {
     return (
         <div className='collection-page-component'>
-            <div className='collection-page-header'>
-                <CollectionImageInfo />
-                <div className='collection-page-user-block'>
-                    <UserProfile />
-                    <CollectionDescription />
-                </div>
+
+
+            <div className="background-element">
+                <img src={'image1.jpg'} alt="Collection" />
             </div>
-            <div className='collection-page-body'>
-                <PlaceInfo
-                    placeName="City Park"
-                    placeType="Park"
-                    googleMapsLink="https://maps.google.com/?q=City+Park"
-                    description="Visiting a waterfall is on almost every traveler’s wish list. But for those heading to the northwestern part of Costa Rica, especially the beaches of Guanacaste, finding one of these magnificent cascades can be difficult. That’s because this region of the country is known to be more flat and dry, not exactly ideal conditions for a waterfall. Luckily though, there are some spectacular waterfalls hidden away. In this post, we’ll tell you about one gem near Liberia, the Llanos de Cortez Waterfall."
-                />
-                <PlaceInfo
-                    placeName="City Park"
-                    placeType="Park"
-                    googleMapsLink="https://maps.google.com/?q=City+Park"
-                    description="Visiting a waterfall is on almost every traveler’s wish list. But for those heading to the northwestern part of Costa Rica, especially the beaches of Guanacaste, finding one of these magnificent cascades can be difficult. That’s because this region of the country is known to be more flat and dry, not exactly ideal conditions for a waterfall. Luckily though, there are some spectacular waterfalls hidden away. In this post, we’ll tell you about one gem near Liberia, the Llanos de Cortez Waterfall."
-                />
-                <PlaceInfo
-                    placeName="City Park"
-                    placeType="Park"
-                    googleMapsLink="https://maps.google.com/?q=City+Park"
-                    description="Visiting a waterfall is on almost every traveler’s wish list. But for those heading to the northwestern part of Costa Rica, especially the beaches of Guanacaste, finding one of these magnificent cascades can be difficult. That’s because this region of the country is known to be more flat and dry, not exactly ideal conditions for a waterfall. Luckily though, there are some spectacular waterfalls hidden away. In this post, we’ll tell you about one gem near Liberia, the Llanos de Cortez Waterfall."
-                />
+            <div style={whiteBGstyle}>
+                <div className='collection-page-header'>
+                    <CollectionImageInfo />
+                    <div className='collection-page-user-block'>
+                        <UserProfile />
+                        <CollectionDescription />
+                    </div>
+                </div>
+                <div className='collection-page-body'>
+                    <PlaceInfo
+                        placeName="City Park"
+                        placeType="Park"
+                        googleMapsLink="https://maps.google.com/?q=City+Park"
+                        description="Visiting a waterfall is on almost every traveler’s wish list. But for those heading to the northwestern part of Costa Rica, especially the beaches of Guanacaste, finding one of these magnificent cascades can be difficult. That’s because this region of the country is known to be more flat and dry, not exactly ideal conditions for a waterfall. Luckily though, there are some spectacular waterfalls hidden away. In this post, we’ll tell you about one gem near Liberia, the Llanos de Cortez Waterfall."
+                    />
+                    <PlaceInfo
+                        placeName="City Park"
+                        placeType="Park"
+                        googleMapsLink="https://maps.google.com/?q=City+Park"
+                        description="Visiting a waterfall is on almost every traveler’s wish list. But for those heading to the northwestern part of Costa Rica, especially the beaches of Guanacaste, finding one of these magnificent cascades can be difficult. That’s because this region of the country is known to be more flat and dry, not exactly ideal conditions for a waterfall. Luckily though, there are some spectacular waterfalls hidden away. In this post, we’ll tell you about one gem near Liberia, the Llanos de Cortez Waterfall."
+                    />
+                    <PlaceInfo
+                        placeName="City Park"
+                        placeType="Park"
+                        googleMapsLink="https://maps.google.com/?q=City+Park"
+                        description="Visiting a waterfall is on almost every traveler’s wish list. But for those heading to the northwestern part of Costa Rica, especially the beaches of Guanacaste, finding one of these magnificent cascades can be difficult. That’s because this region of the country is known to be more flat and dry, not exactly ideal conditions for a waterfall. Luckily though, there are some spectacular waterfalls hidden away. In this post, we’ll tell you about one gem near Liberia, the Llanos de Cortez Waterfall."
+                    />
+                </div>
             </div>
         </div>
     );
